cc5d4337118685c20526fd83ec2889207ef80a55
LadyNick <72218995+LadyNick@users.noreply.github.com>
2021-01-31 19:55:30 -0800

    Update nnava026_lab5_part3.c

--- turnin/nnava026_lab5_part3.c
+++ turnin/nnava026_lab5_part3.c
@@ -12,127 +12,17 @@
 #include "simAVRHeader.h"
 #endif
 
-enum count_states{init, inc_p, inc_r, dec_p, dec_r, reset_p, reset_r, release} count_state;
-
-unsigned char not_pina;
-unsigned char A0;
-unsigned char A1;
-
-void Tick_Count(){
-	switch (count_state){
-		case init:
-			if( A0 && !A1 ){
-				count_state = inc_p;
-			}
-			else if( !A0 && A1){
-				count_state = dec_p;
-			}
-			else if( A0 && A1 ){
-				count_state = reset_p;
-			}
-			break;
-		case inc_p:
-			if( A0 && A1 ){
-				count_state = reset_p;
-			}
-			else{
-				count_state = inc_r;
-			}
-			break;
-		case inc_r: 
-			if( A0 && A1 ){
-				count_state = reset_p;
-			}
-			else if( A0 ){
-				count_state = inc_r;
-			}
-			else if( !A0 ){
-				count_state = release;
-			}
-			break;
-		case dec_p:
-			if( A0 && A1 ) {
-				count_state = reset_p;
-			}
-			else{
-				count_state = dec_r;
-			}
-			break;
-		case dec_r:
-			if(A0 && A1 ){
-				count_state = reset_p;
-			}
-			else if( A1 ){
-				count_state = dec_r;
-			}
-			else if( !A1 ){
-				count_state = release;
-			}
-		case reset_p:
-			count_state = reset_r;
-			break;
-		case reset_r:
-			if( !A0 && !A1 ){
-				count_state = release;
-			}
-			else{
-				count_state = reset_r;
-			}
-			break;
-		case release:
-			if( A0 && !A1 ){
-                                count_state = inc_p;
-                        }
-                        else if( !A0  && A1){
-                                count_state = dec_p;
-                        }
-                        else if( A0 && A1 ){
-                                count_state = reset_p;
-                        }
-                        break;
-		default:
-			count_state = init;
-	}
-
-	switch (count_state){
-		default:
-			break;
-		case init:
-			PORTC = 0x07;
-			break;
-		case inc_p:
-			if((PORTC < 0x09)){
-				PORTC = PORTC + 1;
-			}
-			break;
-		case dec_p:
-			if((PORTC > 0)){
-				PORTC = PORTC - 1;
-			}
-			break;
-		case reset_p:
-			PORTC = 0x00;
-			break;
-	}
-}
-
-
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRC = 0xFF; PORTC = 0x00;
-	DDRA = 0x00; PINA = 0xFF;
+
 
     /* Insert your solution below */
 
 	count_state = init;
 
     while (1){
-	not_pina = ~PINA;
-	A0 = (not_pina & 0x01) > 0;
-	A1 = (not_pina & 0x02) > 0;
 
-	Tick_Count();
     }
     return 1;
 }
