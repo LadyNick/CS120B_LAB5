7e3aff7755b5fb5e479ef133e9d3ee83d055f099
LadyNick <nnikki1013@gmail.com>
2021-01-31 17:58:02 -0800

    edits to exercise 2

--- source/main.c
+++ source/main.c
@@ -14,23 +14,25 @@
 
 enum count_states{init, inc_p, inc_r, dec_p, dec_r, reset_p, reset_r, release} count_state;
 
-
+unsigned char not_pina;
+unsigned char A0;
+unsigned char A1;
 
 void Tick_Count(){
 	switch (count_state){
 		case init:
-			if(((PINA & 0x01) > 0) && !((PINA & 0x02) > 0) ){
+			if( A0 && !A1 ){
 				count_state = inc_p;
 			}
-			else if( !((PINA & 0x01) > 0) && ((PINA & 0x02) > 0)){
+			else if( !A0 && A1){
 				count_state = dec_p;
 			}
-			else if(((PINA & 0x01) > 0) && ((PINA & 0x02) > 0) ){
+			else if( A0 && A1 ){
 				count_state = reset_p;
 			}
 			break;
 		case inc_p:
-			if(((PINA & 0x01) > 0) && ((PINA & 0x02) > 0) ){
+			if( A0 && A1 ){
 				count_state = reset_p;
 			}
 			else{
@@ -38,18 +40,18 @@ void Tick_Count(){
 			}
 			break;
 		case inc_r: 
-			if(((PINA & 0x01) > 0) && ((PINA & 0x02) > 0) ){
+			if( A0 && A1 ){
 				count_state = reset_p;
 			}
-			else if((PINA & 0x01) > 0){
+			else if( A0 ){
 				count_state = inc_r;
 			}
-			else if( !((PINA & 0x01) > 0)){
+			else if( !A0 ){
 				count_state = release;
 			}
 			break;
 		case dec_p:
-			if( ((PINA & 0x01) > 0) && ((PINA & 0x02) > 0) ) {
+			if( A0 && A1 ) {
 				count_state = reset_p;
 			}
 			else{
@@ -57,20 +59,20 @@ void Tick_Count(){
 			}
 			break;
 		case dec_r:
-			if(((PINA & 0x01) > 0) &&((PINA & 0x02) > 0) ){
+			if(A0 && A1 ){
 				count_state = reset_p;
 			}
-			else if((PINA & 0x02) > 0 ){
+			else if( A1 ){
 				count_state = dec_r;
 			}
-			else if( !((PINA & 0x02) > 0) ){
+			else if( !A1 ){
 				count_state = release;
 			}
 		case reset_p:
 			count_state = reset_r;
 			break;
 		case reset_r:
-			if( !((PINA & 0x01) > 0) && !((PINA & 0x02) > 0) ){
+			if( !A0 && !A1 ){
 				count_state = release;
 			}
 			else{
@@ -78,13 +80,13 @@ void Tick_Count(){
 			}
 			break;
 		case release:
-			if( ((PINA & 0x01) > 0) && !((PINA & 0x02) > 0) ){
+			if( A0 && !A1 ){
                                 count_state = inc_p;
                         }
-                        else if( !((PINA & 0x01) > 0) && ((PINA & 0x02) > 0)){
+                        else if( !A0  && A1){
                                 count_state = dec_p;
                         }
-                        else if( ((PINA & 0x01) > 0) && ((PINA & 0x02) > 0) ){
+                        else if( A0 && A1 ){
                                 count_state = reset_p;
                         }
                         break;
@@ -126,6 +128,10 @@ int main(void) {
 	count_state = init;
 
     while (1){
+	not_pina = ~PINA;
+	A0 = (not_pina & 0x01) > 0;
+	A1 = (not_pina & 0x02) > 0;
+
 	Tick_Count();
     }
     return 1;
